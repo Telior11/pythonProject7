8ebbe17c71703cb062fe40ff298244cb4f1855e1
Telior11 <paveltyurin1@yandex.ru>
2023-01-23 13:38:14 +0300

    Update script.py

--- 13.py
+++ 13.py
@@ -1,9 +1,9 @@
 tickets = int(input("Введите количество необходимых билетов: "))
-Visitors = 1
+visitors = 1
 
 sum = 0
-while Visitors <= tickets != 0:
-    age_for_ticket = int(input(f'Укажите для какого возраста приобретается билет № {Visitors} ? '))
+while visitors <= tickets != 0:
+    age_for_ticket = int(input(f'Укажите для какого возраста приобретается билет № {visitors} ? '))
     if age_for_ticket < 18:
         print('Билет бесплатный')
     elif 18 <= age_for_ticket < 25:
@@ -12,11 +12,12 @@ while Visitors <= tickets != 0:
     elif 25 <= age_for_ticket:
         sum += 1390
         print('Стоимость билета: 1390 руб.')
-    Visitors += 1
+    visitors += 1
 
 if tickets > 3:
     sale = sum - ((sum / 100) * 10)
     print(f'Сумма к оплате {sale} руб., применена 10%-ая скидка за покупку более 3-x билетов')
 else:
     print(f'Сумма к оплате {sum} руб.')
+print("")
 print("Хорошего отдыха и ждем вас снова!!!")
